10596033b83413c2d44c61dab9fe0ff607bc3a31
Theron Halsey <theron.halsey@gmail.com>
2018-03-18 14:37:25 -0400

    html done. basic styling. on to JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyBJvfj8MpRlR8roLnpavty5fAlNLfzK93w",
+    authDomain: "trainschedule-b9a12.firebaseapp.com",
+    databaseURL: "https://trainschedule-b9a12.firebaseio.com",
+    projectId: "trainschedule-b9a12",
+    storageBucket: "",
+    messagingSenderId: "17644085006"
+  };
+  firebase.initializeApp(config);
+
+  
